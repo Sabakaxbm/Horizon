e044287c43f902488ef3dd03eeee9a9d308a44e7
Sabaka <soba4kaxbm@mail.ru>
2023-11-23 18:32:58 +0600

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -43,7 +43,7 @@ module.exports = {
       },
       height: {
         'nav-height': '91vh',
-        'nav-height-content': 'calc(100% - 10rem)',
+        'nav-height-content': 'calc(100% - 10.5rem)',
       },
       gridTemplateRows: {
         // Simple 8 row grid
